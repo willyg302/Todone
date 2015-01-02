cd07e4e0c428b4f7370aefc0d10e1ace63503b8f
William Gaul <willyg302@gmail.com>
2015-01-02 10:12:40 -1000

    More idiomatic cljs

--- app/src/todone/core.cljs
+++ app/src/todone/core.cljs
@@ -1,58 +1,61 @@
 (ns todone.core
   (:require [reagent.core :as reagent :refer [atom]]
             [ajax.core :as ajax]
-            [cljs-time.core :as moment]
-            [cljs-time.format :as time-format]))
+            [cljs-time.core :as t]
+            [cljs-time.format :as t-format]))
 
 (enable-console-print!)
 
 
 ;;;; UTILS
 
-; @TODO: This is just a placeholder, actually grab from AJAX
-(def cal (moment/interval (moment/date-time 2014 9 9)
-                          (moment/plus (moment/now) (moment/years 1))))
-
 (defn same-day?
   [a b]
-  (and (= (moment/year a) (moment/year b))
-       (= (moment/month a) (moment/month b))
-       (= (moment/day a) (moment/day b))))
+  (every? true? (map #(= (% a) (% b)) [t/year t/month t/day])))
 
 (defn today?
-  [day]
-  (same-day? (moment/now) day))
+  [date]
+  (same-day? (t/now) date))
 
-; Overrides the incorrect versions in cljs-time
+(defn date-str
+  ([date] (date-str date "MMM d, yyyy"))
+  ([date formatter] (t-format/unparse (t-format/formatter formatter) date)))
 
+(defn interval-str
+  ([{:keys [start end]}]
+    (interval-str start end))
+  ([start end]
+    (if (same-day? start end)
+      (date-str start)
+      (str (date-str start) " \u2013 " (date-str end)))))
+
+; Overrides the incorrect version in cljs-time
 (defn within?
   [interval date]
-  (or (same-day? (moment/start interval) date)
-      (moment/within? interval date)
-      (same-day? (moment/end interval) date)))
+  (or (same-day? (t/start interval) date)
+      (t/within? interval date)
+      (same-day? (t/end interval) date)))
 
+; Overrides the incorrect version in cljs-time
 (defn overlaps?
   [a b]
-  (or (within? a (moment/start b))
-      (within? b (moment/start a))))
-
-(defn interval-string
-  [interval]
-  (let [formatter (time-format/formatter "MMM d, yyyy")
-        format-day (fn [day] (time-format/unparse formatter day))
-        start (moment/start interval)
-        end (moment/end interval)]
-    (if (same-day? start end)
-      (format-day start)
-      (str (format-day start) " \u2013 " (format-day end)))))
+  (or (within? a (t/start b))
+      (within? b (t/start a))))
 
 
+; BEGIN DANGER ZONE
 ; @TODO: SO MANY MAGIC NUMBERS. MUST GET RID OF
+; Also, most of this stuff is temporary until a link to Go is established
 
 ;;;; GLOBAL STATE
 
+; @TODO: This is just a placeholder, actually grab from AJAX
+(def cal (t/interval (t/date-time 2014 9 9)
+                          (t/plus (t/now) (t/years 1))))
+
+
 (def app-state (atom {
-  :selected-days (moment/interval (moment/now) (moment/now))
+  :selected-days (t/interval (t/now) (t/now))
   :days (sorted-map)
   :todos (sorted-map)}))
 
@@ -62,24 +65,27 @@
   (let [id (swap! counter inc)]
     (swap! app-state assoc-in [:todos id] {:id id
                                            :title text
-                                           :interval interval})))
+                                           :interval interval
+                                           :completed false})))
 
-(add-todo "Call mom" (moment/interval (moment/date-time 2014 10 9) (moment/date-time 2014 10 9)))
-(add-todo "Go to the store" (moment/interval (moment/date-time 2014 11 15) (moment/date-time 2014 12 1)))
-(add-todo "This is today" (moment/interval (moment/now) (moment/now)))
+(add-todo "Call mom" (t/interval (t/date-time 2014 10 9) (t/date-time 2014 10 9)))
+(add-todo "Go to the store" (t/interval (t/date-time 2014 11 15) (t/date-time 2014 12 1)))
+(add-todo "This is today" (t/interval (t/now) (t/now)))
 
 
 (defn add-day [d]
   (swap! app-state assoc-in [:days d] {:id d
-                                       :date (moment/plus (moment/start cal) (moment/days d))
+                                       :date (t/plus (t/start cal) (t/days d))
                                        :status (rand-int 3)}))
 
 (defn add-days []
-  (let [numdays (moment/in-days (moment/interval (moment/start cal) (moment/now)))]
+  (let [numdays (t/in-days (t/interval (t/start cal) (t/now)))]
     (doseq [d (range (+ numdays 1))]
       (add-day d))))
 
 (add-days)
+; END DANGER ZONE
+
 
 ;;;; COMPONENTS
 
@@ -93,15 +99,17 @@
                 :on-click #(ajax/POST
                             "/logout"
                             {:handler (fn [response] (js/location.reload))})}
-               "Log Out"]]])
+        "Log Out"]]])
+
+
+;;; DAY
 
 (defn handle-day-click [e m]
-  (if (.-shiftKey e)
-    (let [start (moment/start (@app-state :selected-days))]
-      (swap! app-state assoc :selected-days
-        (moment/interval (moment/earliest start m)
-                         (moment/latest start m))))
-    (swap! app-state assoc :selected-days (moment/interval m m))))
+  (swap! app-state assoc :selected-days
+    (if (.-shiftKey e)
+      (let [start (t/start (@app-state :selected-days))]
+        (t/interval (t/earliest start m) (t/latest start m)))
+      (t/interval m m))))
 
 (defn get-day-color [d m]
   (cond
@@ -111,7 +119,7 @@
         0 "red"
         1 "green"
         "#eeeeee")
-    (moment/after? m (moment/now)) "#888888"
+    (t/after? m (t/now)) "#888888"
     :else "#eeeeee"))
 
 (defn day [d m w p offset]
@@ -121,18 +129,21 @@
           :y (* (rem offset 7) (+ w p))
           :fill (get-day-color d m)
           :data-i m
-          :on-click (fn [e] (handle-day-click e m))}])
+          :on-click #(handle-day-click % m)}])
+
 
 (defn calendar [num-days]
-  (let [start-offset (mod (moment/day-of-week (moment/start cal)) 7)
-        cal-width (- (* 40 (js/Math.ceil (/ (+ num-days start-offset) 7))) 4)]
+  (let [offset (-> cal t/start t/day-of-week (mod 7))
+        cal-width (-> offset (+ num-days) (/ 7) js/Math.ceil (* 40) (- 4))]
     [:div.calendar-bg.container-fluid
       [:div.scroll
         [:svg#calendar {:width cal-width}
           (for [d (range num-days)]
-            ^{:key d} (let [m (moment/plus (moment/start cal) (moment/days d))]
-                        [day d m 36 4 (+ d start-offset)]))]]]))
+            ^{:key d} (let [m (t/plus (t/start cal) (t/days d))]
+                        [day d m 36 4 (+ d offset)]))]]]))
 
+
+;;; TODO LIST
 
 (defn todo-item [item]
   [:div.panel.panel-default
@@ -141,44 +152,41 @@
 
 (defn todo-list []
   [:div#list.col-md-8
-    [:h1.page-header (interval-string (@app-state :selected-days))]
-    (for [d (filter #(overlaps? (@app-state :selected-days) (% :interval)) (vals (@app-state :todos)))]
+    [:h1.page-header (interval-str (@app-state :selected-days))]
+    (for [d (filter #(overlaps? (@app-state :selected-days) (% :interval))
+                    (vals (@app-state :todos)))]
       ^{:key (d :id)} [todo-item d])])
 
+
+;;; SIDEBAR
 
 (defn sidebar-panel [title content]
   [:div.panel.panel-default
     [:div.panel-heading title]
     (into [:div.panel-body] content)])
 
-
-(defn get-longest-chain []
-  (let [p (partition-by #(= (% :status) 1) (vals (@app-state :days)))
-        c (filter #(= ((first %) :status) 1) p)]
-    (first (sort-by count > c))))
-
-(defn get-current-chain []
-  (let [p (partition-by #(= (% :status) 1) (vals (@app-state :days)))
-        c (filter #(= (% :status) 1) (last p))]
-    c))
+(defn chain-info-box [title chain]
+  (let [c (count chain)]
+    [sidebar-panel title
+      [
+        [:h1.text-center (str c " day" (if-not (= c 1) "s"))]
+        (when (pos? c)
+          [:p.text-center (interval-str ((first chain) :date)
+                                        ((last chain) :date))])]]))
 
 (defn sidebar []
-  [:div#sidebar.col-md-4
-    [sidebar-panel "Longest Chain"
-      (let [longest-chain (get-longest-chain)
-            longest-interval (moment/interval ((first longest-chain) :date)
-                                              ((last longest-chain) :date))]
-        [
-          [:h1.text-center (str (count longest-chain) " days")]
-          [:p.text-center (interval-string longest-interval)]])]
-    [sidebar-panel "Current Chain" (let [current-chain (get-current-chain)]
-        [
-          [:h1.text-center (str (count current-chain) " days")]
-          [:p.text-center (if (zero? (count current-chain))
-            "Better get on that"
-            (interval-string (moment/interval ((first current-chain) :date)
-                                              ((last current-chain) :date))))]])]])
+  (let [p (partition-by #(= (% :status) 1) (vals (@app-state :days)))]
+    [:div#sidebar.col-md-4
+      (chain-info-box "Longest Chain"
+                      (->> p
+                           (filter #(= ((first %) :status) 1))
+                           (sort-by count)
+                           last))
+      (chain-info-box "Current Chain"
+                      (filter #(= (% :status) 1) (last p)))]))
 
+
+;;;; MAIN
 
 (defn body []
   [:div#body.container
@@ -189,11 +197,7 @@
 (defn todone-app []
   [:div
     [header]
-    [calendar (moment/in-days cal)]
+    [calendar (t/in-days cal)]
     [body]])
-
-
-
-;;;; MAIN
 
 (reagent/render-component [todone-app] (.getElementById js/document "app"))
